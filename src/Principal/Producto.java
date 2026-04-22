ecce4ea781b23aec3c470275aefcd834890e5380
beisontorres <beison.torres@davinci.edu.ar>
2026-04-22 01:43:21 -0300

    fix: correccion de Clase Producto

--- src/Principal/Producto.java
+++ src/Principal/Producto.java
@@ -5,46 +5,64 @@ import java.util.LinkedList;
 public class Producto {
 
 	//atriubtos
+	protected String nombreProducto;
+	protected String descripcionProducto;
+	protected double precio;
+	protected LinkedList <Proveedor> proveedores;
 	
-	private String nombreProducto;
-	private String descripcion;
-	private double precio;
-	private LinkedList Proveedores;
 	
 	//constructores
-	public Producto(String nombreProducto, String descripcion, double precio, LinkedList proveedores) {
+	public Producto(String nombreProducto, String descripcionProducto, double precio,
+			LinkedList<Proveedor> proveedores) {
 		super();
 		this.nombreProducto = nombreProducto;
-		this.descripcion = descripcion;
+		this.descripcionProducto = descripcionProducto;
 		this.precio = precio;
-		Proveedores = proveedores;
+		this.proveedores = proveedores;
 	}
-	
-	//getters & setters
+
+
+	//Getters y setters
 	public String getNombreProducto() {
 		return nombreProducto;
 	}
 	public void setNombreProducto(String nombreProducto) {
 		this.nombreProducto = nombreProducto;
 	}
-	public String getDescripcion() {
-		return descripcion;
+
+
+	public String getDescripcionProducto() {
+		return descripcionProducto;
 	}
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
+	public void setDescripcionProducto(String descripcionProducto) {
+		this.descripcionProducto = descripcionProducto;
 	}
+
+
 	public double getPrecio() {
 		return precio;
 	}
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
-	public LinkedList getProveedores() {
-		return Proveedores;
+
+
+	public LinkedList<Proveedor> getProveedores() {
+		return proveedores;
 	}
-	public void setProveedores(LinkedList proveedores) {
-		Proveedores = proveedores;
+	public void setProveedores(LinkedList<Proveedor> proveedores) {
+		this.proveedores = proveedores;
 	}
+
+
+	
+	//Metodos
+	@Override
+	public String toString() {
+		return "Producto [nombreProducto=" + nombreProducto + ", descripcionProducto=" + descripcionProducto
+				+ ", precio=" + precio + ", proveedores=" + proveedores + "]";
+	}
+	
 	
 	
 }
